455c38ca002d4032e44aa56df992cf98ab0ad9cf
Dmitry Ovechkin <dmitryovechkin@Dmitrys-iMac-Pro.local>
2025-01-24 07:54:44 +0300

    task 1

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func TaskHandler(w http.ResponseWriter, r *http.Request) {
+	var body requestBody
+	json.NewDecoder(r.Body).Decode(&body)
+	task = body.Message
+	fmt.Fprintln(w, "OK,", task)
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello,", task)
 }
 
 func main() {
 	router := mux.NewRouter()
 	// наше приложение будет слушать запросы на localhost:8080/api/hello
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", TaskHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
